09b88a5721dff198118a2e885746bb09a79a2fc8
Sam <spelton53@haverfordsd.net>
2023-02-16 11:34:09 -0500

    Initial Commit

---
+++ Circle/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Samuel Pelton on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Circle
+//
+//  Created by Samuel Pelton on 2/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
